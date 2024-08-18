cf8cf6bae1039cfa5d7e077a0eb1113c77493b75
Tarang Srivas <37020491+tarang321@users.noreply.github.com>
2024-08-18 22:09:50 +0530

    Add files via upload

---
+++ qr_reader/qr_reader.ino
@@ -0,0 +1,231 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+
+#include "esp_camera.h"
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "quirc.h"
+
+TaskHandle_t QRCodeReader_Task; 
+
+
+#define CAMERA_MODEL_AI_THINKER
+
+#if defined(CAMERA_MODEL_AI_THINKER)
+  #define PWDN_GPIO_NUM     32
+  #define RESET_GPIO_NUM    -1
+  #define XCLK_GPIO_NUM      0
+  #define SIOD_GPIO_NUM     26
+  #define SIOC_GPIO_NUM     27
+  
+  #define Y9_GPIO_NUM       35
+  #define Y8_GPIO_NUM       34
+  #define Y7_GPIO_NUM       39
+  #define Y6_GPIO_NUM       36
+  #define Y5_GPIO_NUM       21
+  #define Y4_GPIO_NUM       19
+  #define Y3_GPIO_NUM       18
+  #define Y2_GPIO_NUM        5
+  #define VSYNC_GPIO_NUM    25
+  #define HREF_GPIO_NUM     23
+  #define PCLK_GPIO_NUM     22
+#else
+  #error "Camera model not selected"
+#endif
+const char * ssid = "🎈";
+const char * password = "1223334444";
+String server = "http://maker.ifttt.com";
+String eventName = "attendance_ss";
+String IFTTT_Key = "0L7pHxMbtxuS-YLoEZ01YD1GBrmah9zblrKC_FV_nf";
+String IFTTTUrl="https://maker.ifttt.com/trigger/attendance_ss/with/key/0L7pHxMbtxuS-YLoEZ01YD1GBrmah9zblrKC_FV_nf";
+char value1;
+char value2;
+char value3;
+void dumpData();
+void sendDataToSheet();
+void QRCodeReader();
+struct QRCodeData
+{
+  bool valid;
+  int dataType;
+  uint8_t payload[1024];
+  int payloadLen;
+};
+
+struct quirc *q = NULL;
+uint8_t *image = NULL;  
+camera_fb_t * fb = NULL;
+struct quirc_code code;
+struct quirc_data data;
+quirc_decode_error_t err;
+struct QRCodeData qrCodeData;  
+String QRCodeResult = "";
+
+void setup() {
+  
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+
+  
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Viola, Connected !!!");
+
+
+  Serial.setDebugOutput(true);
+  Serial.println();
+ 
+  Serial.println("Start configuring and initializing the camera...");
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 10000000;
+  config.pixel_format = PIXFORMAT_GRAYSCALE;
+  config.frame_size = FRAMESIZE_QVGA;
+  config.jpeg_quality = 15;
+  config.fb_count = 1;
+  
+  #if defined(CAMERA_MODEL_ESP_EYE)
+    pinMode(13, INPUT_PULLUP);
+    pinMode(14, INPUT_PULLUP);
+  #endif
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    ESP.restart();
+  }
+  
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_framesize(s, FRAMESIZE_QVGA);
+  
+  Serial.println("Configure and initialize the camera successfully.");
+  Serial.println();
+  
+  xTaskCreatePinnedToCore(
+             QRCodeReader,          /* Task function. */
+             "QRCodeReader_Task",   /* name of task. */
+             10000,                 /* Stack size of task */
+             NULL,                  /* parameter of the task */
+             1,                     /* priority of the task */
+             &QRCodeReader_Task,    /* Task handle to keep track of created task */
+             0);                    /* pin task to core 0 */
+  
+}
+
+void loop() {
+ 
+  
+  
+
+  sendDataToSheet();
+  delay(10000);// put your main code here, to run repeatedly:
+  delay(1);
+}
+
+void QRCodeReader( void * pvParameters ){
+  
+  Serial.println("QRCodeReader is ready.");
+  Serial.print("QRCodeReader running on core ");
+  Serial.println(xPortGetCoreID());
+  Serial.println();
+ 
+  while(1){
+      q = quirc_new();
+      if (q == NULL){
+        Serial.print("can't create quirc object\r\n");  
+        continue;
+      }
+    
+      fb = esp_camera_fb_get();
+      if (!fb)
+      {
+        Serial.println("Camera capture failed");
+        continue;
+      }   
+      
+      quirc_resize(q, fb->width, fb->height);
+      image = quirc_begin(q, NULL, NULL);
+      memcpy(image, fb->buf, fb->len);
+      quirc_end(q);
+      
+      int count = quirc_count(q);
+      if (count > 0) {
+        quirc_extract(q, 0, &code);
+        err = quirc_decode(&code, &data);
+    
+        if (err){
+          Serial.println("Decoding FAILED");
+          QRCodeResult = "Decoding FAILED";
+        } else {
+          Serial.printf("Decoding successful:\n");
+          dumpData(&data);
+        } 
+        Serial.println();
+      } 
+      
+      esp_camera_fb_return(fb);
+      fb = NULL;
+      image = NULL;  
+      quirc_destroy(q);
+  }
+} 
+void dumpData(const struct quirc_data *data)
+{
+  
+  Serial.printf("Payload: %s\n", data->payload);
+  
+  QRCodeResult = (const char *)data->payload;
+ 
+  
+}
+
+void sendDataToSheet(void * pvParameters)
+{
+  String url = server + "/trigger/" + eventName + "/with/key/" + IFTTT_Key + "?value1=" + dumpData(&data) ;  
+  Serial.println(url);
+  //Start to send data to IFTTT
+  HTTPClient http;
+  Serial.print("[HTTP] begin...\n");
+  http.begin(url); //HTTP
+
+  Serial.print("[HTTP] GET...\n");
+  // start connection and send HTTP header
+  int httpCode = http.GET();
+  // httpCode will be negative on error
+  if(httpCode > 0) {
+    // HTTP header has been send and Server response header has been handled
+    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+    // file found at server
+    if(httpCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+      Serial.println(payload);
+    }
+  } else {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+  http.end();
+
+}
